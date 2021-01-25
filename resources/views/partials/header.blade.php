e054a119976a845c4b00a1d0b8b5bffa52b3fe2c
Simona Cinti <simona.cinti2@gmail.com>
2021-01-25 17:41:49 +0100

    homepage route header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
 <header class="mb-5">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">My Blog</a>
+        <a class="navbar-brand" href="{{ route('homepage')}}">My Blog</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
